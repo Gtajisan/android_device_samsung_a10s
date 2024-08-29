a81e8f0cf92eecafe0b22909e5ef0cc3a6e4a063
One Smiled <beksultanbazuov@gmail.com>
2024-08-29 08:12:40 +0000

    a10s: add SEPolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,6 +132,8 @@ VENDOR_SECURITY_PATCH := 2023-02-05
 SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_USING_LEGACY_SELINUX := true
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
